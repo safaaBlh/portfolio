6743849c14130faf6babdedc0ee3dfa4285fe2f5
safaaBlh <safaa.blh213@gmail.com>
2025-05-06 16:57:31 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navbar = document.getElementById('navbar');
-
-menuToggle.addEventListener('click', () => {
-  navbar.classList.toggle('show');
-});
