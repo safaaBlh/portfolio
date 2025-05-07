a76aa31f9271b31a5f90ad7453e91af3c43eef4e
safaaBlh <safaa.blh213@gmail.com>
2025-05-07 02:05:13 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-ةwindow.addEventListener("scroll", function() {
-  const heroSection = document.querySelector('.hero');
-  const heroPosition = heroSection.getBoundingClientRect().top;
-  const screenPosition = window.innerHeight / 1.5;
-
-  if (heroPosition < screenPosition) {
-    document.querySelector('.hero .typewriter').classList.add('visible');
-  }
-});
